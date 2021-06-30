c970cd916ab4a5808f256cee51d4b6a228c4b24b
Naveen K <mailnavn@gmail.com>
2021-06-30 01:34:24 +0100

    Removed libraries not used

--- SnakeAndLadder/DataModel/Ladder.cs
+++ SnakeAndLadder/DataModel/Ladder.cs
@@ -1,10 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace SnakeAndLadder.DataModel
+﻿namespace SnakeAndLadder.DataModel
 {
     public class Ladder
     {
