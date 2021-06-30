b9c2625e90abd122f5b7c6b5278b9217617d5640
Naveen K <mailnavn@gmail.com>
2021-06-30 01:32:51 +0100

    Initial commit adding datamodel, the configuration files and the initial code

---
+++ SnakeAndLadder/DataModel/Board.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SnakeAndLadder.DataModel
+{
+    public class Board
+    {
+        public int NumberOfSquares { get; set; }
+
+        public string Ladders { get; set; }
+
+        public string snakes { get; set; }
+
+    }
+}
